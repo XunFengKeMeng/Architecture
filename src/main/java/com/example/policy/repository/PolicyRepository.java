43f3e66734511bc1dd319235d1cd0f72f2912bef
BuBu <30849626+Dirdream3@users.noreply.github.com>
2024-06-01 14:01:52 +0800

    Delete src/main/java/com/example/policy directory

--- src/main/java/com/example/policy/repository/PolicyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.policy.repository;
-
-import com.example.policy.entity.Policy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
